7ad96b56ccf5809f76ac2f4ec74d1dbccf8b38f9
Alejandro Fernandez <ext.alejandro.fernandez@fdj-gs.com>
2024-08-31 13:20:04 +0200

    Initial commit

---
+++ src/main/java/com/fernandeza/price_finder/PriceFinderApplication.java
@@ -0,0 +1,13 @@
+package com.fernandeza.price_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandeza/price_finder/PriceFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandeza.price_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
